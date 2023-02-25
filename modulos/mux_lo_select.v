7da5229c4fc598f19cfb253670d75929c19f29f7
Felipe Neiva <felipeneivals@gmail.com>
2023-02-25 11:03:18 -0300

    feat: mux HiSelect, LoSelect and MemToReg added

---
+++ modulos/mux_lo_select.v
@@ -0,0 +1,14 @@
+module mux_lo_select(
+    input wire [31:0] in_0, // div low
+    input wire [31:0] in_1, // mult low
+    input wire        select, //Controlado pelo LoSelect
+    output reg [31:0]  out;
+);
+
+always@(*)begin
+    case (select)
+        1'b0: out = in_0;
+        1'b1: out = in_1;
+    endcase
+end
+endmodule
