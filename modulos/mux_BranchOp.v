d23ac4339c817620f85a96dd9eb14cddd1022fca
Frederick <antvenomxd72@gmail.com>
2023-02-25 11:08:42 -0300

    added muxes

---
+++ modulos/mux_BranchOp.v
@@ -0,0 +1,19 @@
+module mux_BranchOp(
+    input  wire  [1:0]  select,
+    input  wire  [31:0] in_00, // EQ
+    input  wire  [31:0] in_01, // ~EQ
+    input  wire  [31:0] in_10, // GT
+    input  wire  [31:0] in_11, // ~GT
+    output wire  [31:0] out;
+);
+
+    always @(*) begin
+        case(select)
+            2'b00 out = in_00;
+            2'b01 out = in_01;
+            2'b10 out = in_10;
+            2'b11 out = in_11;
+        endcase
+
+    end
+endmodule
---
+++ modulos/mux_RegDst.v
@@ -0,0 +1,21 @@
+module mux_RegDst(
+    input  wire  [2:0]  select,
+    input  wire  [4:0] in_000, // rt
+    input  wire  [4:0] in_011, // rd
+    input  wire  [4:0] in_100, // rs
+    output wire  [4:0] out;
+);
+
+    always @(*) begin
+        case(select)
+            3'b000 out = in_000;   
+            3'b001 out = 5'b11101; // 29
+            3'b010 out = 5'b11111; // 31
+            3'b011 out = in_011;
+            3'b100 out = in_100;
+            3'b101 out = 32'b0; // entradas unitilizadas
+            3'b110 out = 32'b0;
+            3'b111 out = 32'b0;
+        endcase
+    end
+endmodule
