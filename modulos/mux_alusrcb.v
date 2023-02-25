c5d55e6070c1baadffca3251ed5d23cd9773c2a5
Ian Gabriel Braga Trinta <leloco61@gmail.com>
2023-02-25 14:57:31 -0300

    Update mux_alusrcb.v

--- modulos/mux_alusrcb.v
+++ modulos/mux_alusrcb.v
@@ -7,10 +7,10 @@ module mux_alusrcb(input wire [31:0] in_00, // Reg_B
   
   always@(*) begin
     case (sel)
-      2'b00: out = reg_B; 
+      2'b00: out = in_00; 
       2'b01: out = 32'b100; // 4
-      2'b10: out = instruction_15_11;
-      2'b11: out = c;
+      2'b10: out = in_10;
+      2'b11: out = in_11;
     endcase
   end
 endmodule
