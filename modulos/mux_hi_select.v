180741ac2d04329272b97088e9abf5f817fbd444
Frederick Almeida <96616444+FrederickAlmeida@users.noreply.github.com>
2023-03-01 00:20:35 -0300

    Update mux_hi_select.v

--- modulos/mux_hi_select.v
+++ modulos/mux_hi_select.v
@@ -2,7 +2,7 @@ module mux_hi_select(
     input wire [31:0] in_0, // div high
     input wire [31:0] in_1, // mult high
     input wire        select, //Controlado pelo HiSelect
-    output reg [31:0]  out;
+    output reg [31:0]  out
 );
 
 always@(*)begin
