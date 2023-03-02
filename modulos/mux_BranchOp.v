ed9b5b413deaf7d28a243a758c4e2335a69a2c2b
Frederick <antvenomxd72@gmail.com>
2023-03-02 20:00:14 -0300

    .

--- modulos/mux_BranchOp.v
+++ modulos/mux_BranchOp.v
@@ -1,10 +1,10 @@
 module mux_BranchOp(
     input  wire  [1:0]  select,
-    input  wire  [31:0] in_00, // EQ
-    input  wire  [31:0] in_01, // ~EQ
-    input  wire  [31:0] in_10, // GT
-    input  wire  [31:0] in_11, // ~GT
-    output wire  [31:0] out
+    input  wire in_00, // EQ
+    input  wire in_01, // ~EQ
+    input  wire in_10, // GT
+    input  wire in_11, // ~GT
+    output wire out
 );
 
     always @(*) begin
