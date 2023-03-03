a698fab1652f8a207ae7f1b8f7f6b7b07d88f345
caiopsd <101722357+caiopsd@users.noreply.github.com>
2023-03-03 00:30:05 -0300

    Update loadSize.v

--- modulos/loadSize.v
+++ modulos/loadSize.v
@@ -7,7 +7,7 @@ module loadSize(
 
     wire [31:0] Aux;
 
-    assign LSout = (LSControl1) ? Aux : LSin_mdr;
     assign Aux   = (LSControl2) ? {24'd0, LSin_mdr[7:0]} : {16'd0, LSin_mdr[15:0]};
+    assign LSout = (LSControl1) ? Aux : LSin_mdr;
 
 endmodule
